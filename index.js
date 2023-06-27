50436444d1aa7ffd8a79cb0e7e9c618b1cc1af29
sawanpatidar75 <sawanpatidar75@gmail.com>
2023-06-28 00:58:13 +0530

    Manage cors and req.body

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
 const nodemailer = require('nodemailer');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const app = express();
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
